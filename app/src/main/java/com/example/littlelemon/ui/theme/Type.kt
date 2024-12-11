6f74ce79b8ac3bd0695991bbe5d27a6acba5217c
bbsoftdev <bbsoftdev@outlook.com>
2024-12-11 09:15:00 +0100

    updated ui typography

--- app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
@@ -2,19 +2,65 @@ package com.example.littlelemon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.littlelemon.R
+
+val Karla = FontFamily(
+    Font(R.font.karla_regular)
+)
+
+val MarkaziText = FontFamily(
+    Font(R.font.markazi_text_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = MarkaziText,
+        fontWeight = FontWeight.Medium,
+        fontSize = 64.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = MarkaziText,
+        fontWeight = FontWeight.Normal,
+        fontSize = 40.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Karla,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 20.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Karla,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Karla,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Karla,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Karla,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 24.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Karla,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
     )
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
