2a0078ce4c2ffb2a524870c3ce0b1778dfd88a56
bbsoftdev <bbsoftdev@outlook.com>
2024-12-13 16:26:43 +0100

    added hero banner to home screen

--- app/src/main/java/com/example/littlelemon/composables/Home.kt
+++ app/src/main/java/com/example/littlelemon/composables/Home.kt
@@ -1,27 +1,37 @@
 package com.example.littlelemon.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.R
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import com.example.littlelemon.ui.theme.spacing
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun Home(navController: NavController) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         Header(navController)
         Column(
@@ -29,10 +39,45 @@ fun Home(navController: NavController) {
                 .fillMaxWidth()
                 .fillMaxHeight(0.4f)
                 .background(MaterialTheme.colorScheme.primary)
+                .padding(horizontal = MaterialTheme.spacing.medium)
         ) {
-            Text(
-                text = stringResource(R.string.little_lemon)
-            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ){
+                Text(
+                    text = stringResource(R.string.restaurant_name),
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.secondary,
+                )
+                Text(
+                    text = stringResource(R.string.restaurant_location),
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier.padding(top = MaterialTheme.spacing.extreme)
+                )
+                Text(
+                    text = stringResource(R.string.restaurant_description),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .fillMaxWidth(.6f)
+                        .padding(top = 40.dp)
+                )
+                Image(
+                    painter = painterResource(R.drawable.hero_image),
+                    contentDescription = stringResource(R.string.banner_image),
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .fillMaxWidth(.35f)
+                        .fillMaxHeight(.45f)
+                        .clip(RoundedCornerShape(MaterialTheme.spacing.small))
+                        .align(Alignment.CenterEnd)
+                )
+            }
+
 
         }
     }
--- app/src/main/java/com/example/littlelemon/ui/theme/Spacing.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Spacing.kt
@@ -13,7 +13,8 @@ data class Spacing(
     val small: Dp = 8.dp,
     val medium: Dp = 16.dp,
     val large: Dp = 24.dp,
-    val extraLarge: Dp = 32.dp
+    val extraLarge: Dp = 32.dp,
+    val extreme: Dp = 60.dp
 )
 
 val LocalSpacing = compositionLocalOf { Spacing() }
